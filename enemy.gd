d9cf121403d70bd8689ec76013dd2d7f606c7190
kamrancouture <65361792+kamrancouture@users.noreply.github.com>
2024-12-21 21:45:12 -0800

    enemy follow player and world tscn created

--- enemy.gd
+++ enemy.gd
@@ -2,8 +2,10 @@ extends CharacterBody2D
 
 var speed = 300
 @onready var navigation = $NavigationAgent2D
+@onready var player = get_parent().get_node("Player")
 
 func _physics_process(delta):
-	pass
-	#navigation.target_position = (get the players position)
-	
+	look_at(player.global_position)
+	navigation.target_position = player.global_position
+	velocity = global_position.direction_to(navigation.get_next_path_position())*speed
+	move_and_slide()

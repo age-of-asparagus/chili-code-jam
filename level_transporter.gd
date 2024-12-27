039d01549b020fa4ef03c4452c0e007d3d936aac
Tylere Couture <tylerecouture@gmail.com>
2024-12-27 09:37:38 -0800

    Elevator door animation and sound and awesomeness

--- level_transporter.gd
+++ level_transporter.gd
@@ -4,4 +4,6 @@ var levels = ["res://Levels/Level0.tscn","res://Levels/level1.tscn"]
 
 func _on_body_entered(body):
 	Global.level += 1
+	$AnimationPlayer.play("DoorClose")
+	await $AnimationPlayer.animation_finished
 	get_tree().change_scene_to_file(levels[Global.level])

624b99d96189071fa1ef99fcfe6925e4c3725356
kamrancouture <65361792+kamrancouture@users.noreply.github.com>
2024-12-27 20:39:36 -0800

    restart on same level

--- game_over.gd
+++ game_over.gd
@@ -1,5 +1,8 @@
 extends Control
+
 @onready var click_sound = $AudioStreamPlayerClick
+
+var levels = ["res://Levels/Level0.tscn","res://Levels/level1.tscn","res://Levels/level2.tscn","res://Levels/level3.tscn","res://win_screen.tscn"]
 
 func _on_menu_button_pressed():
 	click_sound.play()
@@ -11,4 +14,4 @@ func _on_retry_button_pressed():
 	click_sound.play()
 	await click_sound.finished
 	Global.game_over = false
-	get_tree().change_scene_to_file("res://Levels/Level0.tscn")
+	get_tree().change_scene_to_file(levels[Global.level])

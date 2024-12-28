fdd2c6911e6d77ee8bc29f8f63893a5a5f77b325
Tylere Couture <tylerecouture@gmail.com>
2024-12-28 14:08:13 -0800

    fix win and lose screens

--- game_over.gd
+++ game_over.gd
@@ -5,15 +5,19 @@ extends Control
 var levels = ["res://Levels/Level0.tscn","res://Levels/level1.tscn","res://Levels/level2.tscn","res://Levels/level3.tscn","res://Levels/level4.tscn","res://win_screen.tscn"]
 
 func _on_menu_button_pressed():
+	Global.player_health = 10
+	Global.flashlight_battery = 100
 	click_sound.play()
 	await click_sound.finished
 	Global.game_over = false
 	get_tree().change_scene_to_file("res://start_screen.tscn")
 	
 
-func _on_menu_button_2_pressed() -> void:
+func _on_retry_button_pressed() -> void:
+	Global.player_health = 10
+	Global.flashlight_battery = 100
 	click_sound.play()
 	await click_sound.finished
 	Global.game_over = false
-	get_tree().change_scene_to_file("res://credits.tscn")
+	get_tree().change_scene_to_file(levels[Global.level])
 	
--- win.gd
+++ win.gd
@@ -2,13 +2,17 @@ extends Control
 @onready var click_sound = $AudioStreamPlayerClick
 
 func _on_menu_button_pressed():
+	Global.player_health = 10
+	Global.flashlight_battery = 100
 	click_sound.play()
 	await click_sound.finished
 	Global.game_over = false
 	get_tree().change_scene_to_file("res://start_screen.tscn")
 	
-func _on_retry_button_pressed():
+func _on_menu_button_2_pressed() -> void:
+	Global.player_health = 10
+	Global.flashlight_battery = 100
 	click_sound.play()
 	await click_sound.finished
 	Global.game_over = false
-	get_tree().change_scene_to_file("res://Levels/Level0.tscn")
+	get_tree().change_scene_to_file("res://credits.tscn")

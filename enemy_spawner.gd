7a84f874a433a74ad29881c43a07de5531cbbd5f
kamrancouture <65361792+kamrancouture@users.noreply.github.com>
2024-12-28 00:11:01 -0800

    enemy spawners working and levels in order

---
+++ enemy_spawner.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+enum EnemyType {RED, GREEN, BLUE, RANDOM}
+@export var enemy_spawn_type : EnemyType
+@export var spawn_rate : float 
+
+var can_spawn = true
+
+var Enemy = preload("res://enemy.tscn")
+var current_enemy
+func _ready():
+	
+	$spawn_time.wait_time = spawn_rate
+
+func _on_spawn_time_timeout():
+	if can_spawn:
+		if current_enemy == null or current_enemy.attacking == true:
+			current_enemy = Enemy.instantiate()
+			print(current_enemy)
+			current_enemy.enemy_type = enemy_spawn_type
+			current_enemy.global_position = global_position
+			get_parent().find_child("Enemies").add_child(current_enemy)
+
+
+func _on_visible_on_screen_notifier_2d_screen_entered():
+	print("hi")
+	can_spawn = false
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	can_spawn = true
+	print("hii")
--- level_transporter.gd
+++ level_transporter.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-var levels = ["res://Levels/Level0.tscn","res://Levels/level1.tscn","res://Levels/level2.tscn","res://Levels/level3.tscn","res://win_screen.tscn"]
+var levels = ["res://Levels/Level0.tscn","res://Levels/level1.tscn","res://Levels/level2.tscn","res://Levels/level3.tscn","res://Levels/level4.tscn","res://win_screen.tscn"]
 
 func _on_body_entered(body):
 	Global.level_completed = true

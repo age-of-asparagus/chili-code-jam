1ccf432488a9002609420d03161ec21e0acf8f97
Tylere Couture <tylerecouture@gmail.com>
2024-12-28 13:33:59 -0800

    lvl 4 restart works

--- game_over.gd
+++ game_over.gd
@@ -2,7 +2,7 @@ extends Control
 
 @onready var click_sound = $AudioStreamPlayerClick
 
-var levels = ["res://Levels/Level0.tscn","res://Levels/level1.tscn","res://Levels/level2.tscn","res://Levels/level3.tscn","res://win_screen.tscn"]
+var levels = ["res://Levels/Level0.tscn","res://Levels/level1.tscn","res://Levels/level2.tscn","res://Levels/level3.tscn","res://Levels/level4.tscn","res://win_screen.tscn"]
 
 func _on_menu_button_pressed():
 	click_sound.play()

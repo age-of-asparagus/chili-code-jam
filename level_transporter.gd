9b9fda09be84cf2202784a1b0b68705442ecfdbb
kamrancouture <65361792+kamrancouture@users.noreply.github.com>
2024-12-27 23:14:37 -0800

    restart with full health and energy fixed

--- level_transporter.gd
+++ level_transporter.gd
@@ -3,10 +3,10 @@ extends Area2D
 var levels = ["res://Levels/Level0.tscn","res://Levels/level1.tscn","res://Levels/level2.tscn","res://Levels/level3.tscn","res://win_screen.tscn"]
 
 func _on_body_entered(body):
-	Global.player_health = 10
-	Global.flashlight_battery = 100
 	Global.level_completed = true
 	Global.level += 1
 	$AnimationPlayer.play("DoorClose")
 	await $AnimationPlayer.animation_finished
+	Global.player_health = 10
+	Global.flashlight_battery = 100
 	get_tree().change_scene_to_file(levels[Global.level])
